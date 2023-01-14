7d37fb95d9e991675a36f2fa0323e6658bcd5139
Parselian (Vyacheslav) <vyacheslav.job@yandex.ru>
2023-01-14 23:08:23 +0300

    I`ve just made the first HW of React course

--- dev/index.js
+++ dev/index.js
@@ -1,1 +1,42 @@
-console.log('hello wohyfrretgretd')
+import React from 'react'
+import * as reactDom from 'react-dom/client'
+
+/* JSX разметка
+  <div>
+    <h1 className="title">Title</h1>
+    <hr/>
+    <div className="text" onClick={}>Text</div>
+  </div>
+* */
+
+/* , которая компилируется в след. React код:
+* React.createElement('div', {}, [
+*   React.createElement('h1', {className: 'title'}, ['Title'])
+*   React.createElement('hr', {}, [])
+*   React.createElement('div', {className: 'text', onClick: {}}, ['Text'])
+* ])
+* */
+
+import UserCard from "./UserCard";
+import CounterClass from "./CounterClass";
+import CounterFn from "./CounterFn"
+
+const root = reactDom.createRoot(document.getElementById('app')) // CreateRoot is a standard since React 18.0.0 version
+root.render(
+  <>
+    <h1>Hello, world</h1>
+    <hr/>
+      <h2>Class counter min=2 max=7</h2>
+    <CounterClass min={2} max={7}/>
+      <h2>Class counter min=0 max=4</h2>
+    <CounterClass min={-100} max={4}/>
+    <hr/>
+      <h2>Function counter min=1 max=6</h2>
+    <CounterFn min={1} max={6}/>
+    <hr/>
+    <UserCard name="Alex" age="20" profession="merchant"/>
+    <UserCard name="Bob" age="41" profession="clerk"/>
+    <UserCard name="Sara" age="27" profession="Medic"/>
+  </>
+)
+
