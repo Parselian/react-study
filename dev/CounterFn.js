05ccf9478a0657dbe6d4ee095526f898a9fad975
Parselian (Vyacheslav) <vyacheslav.job@yandex.ru>
2023-01-20 21:50:48 +0300

    refactor counter functions

--- dev/CounterFn.js
+++ dev/CounterFn.js
@@ -4,46 +4,21 @@ import PropTypes from "prop-types"
 export default function CounterFn({ min, max }) {
   let [counter, setCounter] = useState(min)
 
-  function inc() {
-    if (counter < max) {
-      setCounter( counter + 1)
-    }
-  }
-
-  function dec() {
-    if (counter > min) {
-      setCounter(counter - 1)
-    }
+  //Min = 1 Max = 10 Counter = 12
+  function validateIncome(counter) {
+    const validNum = Math.max(min, Math.min(max, counter))
+    setCounter(validNum)
   }
 
   function updateInput(e) {
-    const value = e.target.value
-    let result
-
-    if (value >= min && value <= max) {
-      result = parseInt(e.target.value)
-    }
-
-    if (value < min) {
-      result = min
-    }
-
-    if (value > max) {
-      result = max
-    }
-
-    if (isNaN(value) || value === '') {
-      result = value
-    }
-
-    setCounter(result)
+    if (!isNaN(+e.target.value)) validateIncome(+e.target.value)
   }
 
   return(
     <div>
-      <button onClick={ () => { dec() } }>-</button>
+      <button onClick={ () => { validateIncome(counter - 1) } }>-</button>
       <input onChange={ updateInput } value={ counter }/>
-      <button onClick={ () => { inc() } }>+</button>
+      <button onClick={ () => { validateIncome(counter + 1) } }>+</button>
     </div>
   )
 }
